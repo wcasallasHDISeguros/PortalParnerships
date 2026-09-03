be9f19c0a78d9b17f2dce9126390ceb7403f40b4
wilson.casallas@outsourcing.hdiseguros.com.co <wilson.casallas@outsourcing.hdiseguros.com.co>
2026-09-03 15:06:23 -0500

    Version6 2026-09-03

--- Query_Primas.sql
+++ Query_Primas.sql
@@ -499,6 +499,35 @@ END;
 
 
 
+-------------------------------------------------------------------------------
+------------------------------funcion F_DESAGENTE---------------------------------
+-------------------------------------------------------------------------------
 
+CREATE OR REPLACE FUNCTION AXIS."F_DESAGENTE" (
+   pcagente   IN       NUMBER,
+   ptnombre   IN OUT   VARCHAR2
+)
+   RETURN NUMBER AUTHID CURRENT_USER
+IS
+/***********************************************************************
+    F_AGENTE: Nombre del Agente.
+    ALLIBMFM
+***********************************************************************/
+   num_err       NUMBER;
+BEGIN
+
+   num_err := 100504;                                     -- Agent inexistent
+   SELECT f_nombre (sperson, 1, null)
+     INTO ptnombre
+     FROM agentes
+    WHERE cagente = pcagente;
+
+--   num_err := 100534;                                    -- Persona inexistent
+   RETURN 0;
+EXCEPTION
+   WHEN OTHERS
+   THEN
+      RETURN num_err;
+END;  
 
 
