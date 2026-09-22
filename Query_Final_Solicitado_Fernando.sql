8eaec536d9cfab4452ffcc328deb5350ff44e923
wilson.casallas@outsourcing.hdiseguros.com.co <wilson.casallas@outsourcing.hdiseguros.com.co>
2026-09-22 13:42:03 -0500

    Version7 2026-09-22

--- Query_Final_Solicitado_Fernando.sql
+++ Query_Final_Solicitado_Fernando.sql
@@ -792,7 +792,7 @@ autriesgos_ultimo AS (
         cer.ncertif AS certificado_asegurado,
         cer.sseguro AS sseguro_certificado,
         dv.tatribu AS estado_certificado,
-        ar.cversion AS cod_fasecolda
+        ar.cversion AS cod_fasecolda,
         COALESCE(pc.prima_emitida, 0) + COALESCE(pc.impuestos, 0) AS prima_total
     FROM gde_adp_ods.axis_seguros car
     INNER JOIN gde_adp_ods.axis_ramos r ON r.cramo = car.cramo AND r.cidioma = 8
